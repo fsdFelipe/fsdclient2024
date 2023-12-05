46ccda79cf08a833a839bf0ebd61bf85f27bd4ae
fsdFelipe <devfsdfelipe@gmail.com>
2023-12-05 20:11:44 -0300

    Editando componente Hero - AnimatedTitle - layout finalizado

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,96 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import {
     FaFacebookSquare, FaInstagramSquare, FaTwitterSquare,
     FaYoutube, FaKickstarter, FaGithubSquare
 } from "react-icons/fa";
+import { motion } from 'framer-motion'
+import AnimatedTitle from './animations/AnimatedTitle';
+import Link from 'next/link';
 
-const Hero = () => {
+const Hero: React.FC = () => {
     return (
         <section className='w-full bg-blue-300 min-h-screen mt-16 lg:mt-0 items-center justify-center lg:p-12'>
             <div className='w-full lg:flex items-center justify-center text-white border-[1px] border-red-500'>
                 <div className='w-full flex-col lg:w-1/2 p-8 items-center justify-center border-[1px] border-emerald-400'>
-                    <div className='p-5 text-white'>
-                        <h2 className='text-4xl sm:text-5xl font-bold'>Bem Vindo !!!</h2>
-                        <p className='py-5 text-xl'>Conheça minha caminhada para ser um desenvolvedor web</p>
-                        <button className='px-8 py-2 border'>Book</button>
+                    <div className='p-2 text-white'>
+                        <AnimatedTitle text="Bem Vindo !!!" />
+                        <motion.p
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                scale: [1, 1.1, 1], opacity: 1,
+                                transition: {
+                                    duration: 0.6,
+                                },
+                            }
+                            }
+                            className='py-5 text-2xl'
+                        >Conheça minha caminhada para ser um desenvolvedor web
+                        </motion.p>
+                        <motion.button
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                scale: [1, 1.1, 1], opacity: 1,
+                                transition: {
+                                    duration: 0.6,
+                                    delay: 0.7
+                                },
+                            }
+                            }
+                            className='px-8 py-2 border hover:border-red-600'>
+                            <Link href='pages/contato'>Contato</Link>
+                        </motion.button>
                     </div>
-                    <div className='w-full hidden md:flex items-center justify-between p-6'>
-                        <FaFacebookSquare className='w-9 h-9' />
-                        <FaInstagramSquare className='w-9 h-9' />
-                        <FaTwitterSquare className='w-9 h-9' />
-                        <FaYoutube className='w-9 h-9' />
-                        <FaKickstarter className='w-9 h-9' />
-                        <FaGithubSquare className='w-9 h-9' />
+                    <div className='w-full xl:w-4/5 mx-auto hidden lg:flex items-center justify-between p-6'>
+                        <motion.a
+                            href='https://www.facebook.com/profile.php?id=61553821538443'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaFacebookSquare className='w-9 h-9 text-blue-500' />
+                        </motion.a>
+                        <motion.a
+                            href='https://www.instagram.com/kodd07/'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaInstagramSquare className='w-9 h-9 text-red-300' />
+                        </motion.a>
+                        <motion.a
+                            href='/'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaTwitterSquare className='w-9 h-9 text-blue-800' />
+                        </motion.a>
+                        <motion.a
+                            href='https://www.youtube.com/@meucaminhodev'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaYoutube className='w-9 h-9 text-red-500' />
+                        </motion.a>
+                        <motion.a
+                            href='https://kick.com/tchiu'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaKickstarter className='w-9 h-9 text-green-500' />
+                        </motion.a>
+                        <motion.a
+                            href='https://github.com/fsdFelipe'
+                            target={'_blank'}
+                            whileHover={{ y: -4, scale: 1.2 }}
+                            whileTap={{ scale: 0.9 }}
+                        >
+                            <FaGithubSquare className='w-9 h-9 text-black' />
+                        </motion.a>
                     </div>
                 </div>
                 <div className='w-full lg:w-1/2 flex items-center justify-center border-[1px] border-yellow-400 p-8'>
---
+++ components/animations/AnimatedTitle.tsx
@@ -0,0 +1,65 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+
+interface AnimatedTitleProps {
+    text: string;
+}
+
+const texto = {
+    initial: {
+        opacity: 1,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            staggerChildren: 0.08,
+        },
+    },
+};
+
+const letra = {
+    initial: {
+        opacity: 0,
+        scale: 1, // Add initial scale value
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        scale: [1, 1.2, 0.8, 1.1, 1], // Add an array of scale values for bounce effect
+        transition: {
+            duration: 1,
+        },
+    },
+};
+
+const AnimatedTitle: React.FC<AnimatedTitleProps> = ({ text }) => {
+    return (
+        <div className='w-full mx-auto flex items-center justify-center overflow-hidden'>
+            <motion.h1
+                className='inline-block w-full text-slate-700 font-extrabold text-2xl lg:text-3xl'
+                variants={texto}
+                initial='initial'
+                animate='animate'
+            >
+                {text.split('').map((word, index) => (
+                    <motion.span key={word + '-' + index}
+                        className='inline-block hover:text-emerald-400'
+                        variants={letra}
+                        whileHover={{
+                            scale: [1, 1.3, 1],
+                            transition: {
+                                duration: 0.6,
+                            },
+                        }
+                        }>
+                        {word}&nbsp;
+                    </motion.span>
+                ))}
+            </motion.h1>
+        </div>
+    );
+};
+
+export default AnimatedTitle;
