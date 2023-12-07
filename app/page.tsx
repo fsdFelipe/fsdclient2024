9e2475ed2b1674bebc049955cab065171d8ba5e5
fsdFelipe <devfsdfelipe@gmail.com>
2023-12-07 15:21:49 -0300

    Adicionado componente Destaques

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Destaques from '@/components/Destaques'
 import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
     <main className="w-full py-16">
       <Hero />
-      <Hero />
-      <Hero />
+      <Destaques />
     </main>
   )
 }
---
+++ components/Destaques.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AnimatedTitle from './animations/AnimatedTitle'
+
+const Destaques = () => {
+    return (
+        <section>
+            <AnimatedTitle text='Projetos em Destaques' />
+        </section>
+    )
+}
+
+export default Destaques
